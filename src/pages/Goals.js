2365e7e2ad342241c084d29d3f545bc04c85a9fa
CCLancaster <connieclancaster@gmail.com>
2020-04-16 21:23:53 -0700

    started goals page, more to go

---
+++ src/pages/Goals.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Goals(props) {
+
+    return (
+        <div className="buttons">
+            <div className="violet">
+                Journal Entry
+            </div>
+            <div className="indigo">
+                Meditation
+            </div>
+            <div className="blue">
+                Reach Out to a Human
+            </div>
+            <div className="green">
+
+            </div>
+            <div className="yellow">
+
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
