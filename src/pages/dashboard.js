d617d9aac3e1b8ec27c782db0853ebaa822ca41b
CCLancaster <connieclancaster@gmail.com>
2020-04-17 02:05:26 -0700

    filled in dashboard content

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import SettingsIcon from '@material-ui/icons/Settings';
+import EditIcon from '@material-ui/icons/Edit';
 
 function Dashboard(props) {
     return (
-        <div className="content">
-            <h1>This is the Dashboard</h1>
+        <div className="dashboard">
+            <div className="user">
+                <div>
+                    <img src="#" />
+                </div>
+                <div>
+                    <h1>This is the Dashboard</h1>
+                    <p>{SettingsIcon}  {EditIcon}</p>
+                </div>
+            </div>
+            <div className="main">
+                <div className="levels">
+                    This is where the levels go.
+                </div>
+                <div className="dashtiles">
+                    <div className="tile"><Link to="/journal">Journal Entries</Link></div>
+                    <div className="tile"><Link to="/goals">Today's Goals</Link></div>
+                    <div className="tile"><Link to="/categories">Categories</Link></div>
+                    <div className="tile"><Link to="/achievements">Achievements</Link></div>
+                </div>
+            </div>
+            <div className="stats">
+                <h3>Level 3</h3>
+                <p>you have 6 tasks to complete to level up</p>
+            </div>
         </div>
     );
 }
